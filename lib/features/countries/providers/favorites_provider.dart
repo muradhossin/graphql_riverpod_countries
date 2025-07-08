0455c5ca2085be8a0261c17b3eaa991f54046ddd
muradhossin <murad15-2547@diu.edu.bd>
2025-07-08 20:31:37 +0600

    feat: persist favorite countries locally using shared_preferences

--- lib/features/countries/providers/favorites_provider.dart
+++ lib/features/countries/providers/favorites_provider.dart
@@ -1,11 +1,15 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 final favoritesProvider = StateNotifierProvider<FavoritesNotifier, Set<String>>(
   (ref) => FavoritesNotifier(),
 );
 
 class FavoritesNotifier extends StateNotifier<Set<String>> {
-  FavoritesNotifier() : super({});
+  static const String _prefsKey = 'favoriteCountries';
+  FavoritesNotifier() : super({}) {
+    _loadFavorites();
+  }
 
   void toggleFavorite(String countryCode) {
     if(state.contains(countryCode)) {
@@ -17,6 +21,18 @@ class FavoritesNotifier extends StateNotifier<Set<String>> {
       newSet.add(countryCode);
       state = newSet;
     }
+    _saveFavorites();
+  }
+
+  Future<void> _loadFavorites() async {
+    final prefs = await SharedPreferences.getInstance();
+    final saved = prefs.getStringList(_prefsKey) ?? [];
+    state = saved.toSet();
+  }
+
+  Future<void> _saveFavorites() async {
+    final prefs = await SharedPreferences.getInstance();
+    prefs.setStringList(_prefsKey, state.toList());
   }
 
 }
