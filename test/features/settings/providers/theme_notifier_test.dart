e8c390d39303105b5a77fa54b65d6be4f1954724
muradhossin <murad15-2547@diu.edu.bd>
2025-07-10 20:33:20 +0600

    test: add unit tests for ThemeNotifier

---
+++ test/features/settings/providers/theme_notifier_test.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:graphql_riverpod_countries/features/settings/providers/theme_provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+void main() {
+  TestWidgetsFlutterBinding.ensureInitialized();
+
+  group('ThemeNotifier', () {
+   late ThemeNotifier notifier;
+
+   setUp(() async {
+     SharedPreferences.setMockInitialValues({});
+     notifier = ThemeNotifier();
+     await Future.delayed(const Duration(milliseconds: 100));
+   });
+
+   test('toggle changes state to true', () {
+    notifier.toggle(true);
+    expect(notifier.state, isTrue);
+   });
+
+   test('toggle changes state back to false', () {
+     notifier.toggle(true);
+     expect(notifier.state, isTrue);
+
+     notifier.toggle(false);
+     expect(notifier.state, isFalse);
+   });
+
+
+  });
+}
